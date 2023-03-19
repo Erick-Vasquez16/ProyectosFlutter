ea06b0461ac9d18195d3740eeb68e360cb781b5e
Erick-Vasquez16 <73488888+Erick-Vasquez16@users.noreply.github.com>
2023-03-18 20:59:40 -0600

    20:59

---
+++ Parcial2/flutter_parcial_2/lib/json/productos.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
--- Parcial2/flutter_parcial_2/lib/pantallas/menu/kitchen.dart
+++ Parcial2/flutter_parcial_2/lib/pantallas/menu/kitchen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 class Kitchen extends StatelessWidget {
   const Kitchen({super.key});
